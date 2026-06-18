9c7a59ef169e8fec5566671b15df9f94b27555ac
2014000002-AllanGonzalez <a.gonzalez.5ddb@gmail.com>
2026-06-18 11:50:50 -0600

    C#

---
+++ Filas y columnas C#.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int fila = 3;
+        int columna = 5;
+        int i = 0; int j = 0;
+        int[,] matriz = new int[3, 5];
+        for (i = 0; i < fila; i++)
+        {
+            for (j = 0; j < columna; j++)
+            {
+                Console.WriteLine("Ingresa el numero que quieres guardar en la fila: " + i + ", columna: " + j + " =");
+                
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+        for (i = 0; i < fila; i++)
+        {
+            for(j = 0;j < columna; j++)
+            {
+                Console.WriteLine("Fila: " + i + ", Columna: " + j + " =" + matriz[i, j]);
+            }
+        }
+    }
+}
